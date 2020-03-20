4739c7a8cb9fc28ac7d7945f40ffcba44e767973
unknown <anggikhusnurida00@gmail.com>
2020-03-20 16:38:10 +0700

    latihan1

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <tittle> 
+        Belajar PHP 
+    </tittle>
+</head>
+<body>
+    <h1>
+        Selamat datang <?php echo "masukkan nama anda";?>
+    </h1>
+</body>
+</html>
