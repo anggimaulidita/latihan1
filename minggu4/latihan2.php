413b890141e5e33c83b34a68f80e00399de7abae
unknown <anggikhusnurida00@gmail.com>
2020-03-28 21:11:21 +0700

    praktikumcontrolflow

--- minggu4/latihan2.php
+++ minggu4/latihan2.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang = "en">
 <head>
-	<meta charset="UTF-8">
-	<title>Latihan looping</title>
+    <meta charset = "UTF - 8">
+    <title>Latihan Looping</title>
 </head>
 <body>
-	<h2>membuat perulangan tabel</h2>
-	<table border="1" cellpadding="10" cellspacing="0">
-		<?php
-		for ($b =1;$b<=10;$b++){#b baris
-			echo "<tr>";
-			for ($k=1; $k <=8 ; $k++) {#k kolom
-				echo "<td> $b,$k </td>";
-			}
-			echo "<tr>";
-		}
-		?>
-	</table>
+    <h2>Membuat Perulangan Tabel</h2>
+    <table border = "1" cellpadding = "10" cellspacing = "0">
+
+        <?php
+            for ( $b = 1; $b <= 10; $b++ ) { # $b = untuk baris
+
+                    if ($b % 2 == 0){ // kondisi
+                        echo "<tr>";    // konidisi true
+                    }
+                    else {
+                        echo "<tr bgcolor = #F08080>"; // konddisi false
+                    }
+
+                for ( $k = 1; $k <= 8; $k++ ) { # $k = untuk kolom
+                    echo "<td> $b , $k </td>";
+                }
+                echo "</tr>";
+            }
+        ?>
+
+    </table>
 </body>
 </html>
