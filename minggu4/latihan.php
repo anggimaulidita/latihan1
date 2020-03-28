8d9566318a18f854601a55a5d6071d26f5e4a2c8
unknown <anggikhusnurida00@gmail.com>
2020-03-28 16:33:16 +0700

    perintah2

--- minggu4/latihan.php
+++ minggu4/latihan.php
@@ -1,7 +1,8 @@
 <?php
+	echo "<h2> Menggunakan Perulangan For </h2>";
 
-for ($i = 0; $i < 5; $i++){
-	echo "Hello Word ! <br>";
+for ($i = 1; $i < 6; $i++){
+	echo "Hai,saya urutan ke-$i <br>";
 
 }
 
