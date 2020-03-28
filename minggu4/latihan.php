509f7bbef0bff41d27e9420fc0684e01593650b0
unknown <anggikhusnurida00@gmail.com>
2020-03-28 16:58:57 +0700

    perintah4

--- minggu4/latihan.php
+++ minggu4/latihan.php
@@ -1,9 +1,11 @@
 <?php
+
+echo "<h2>Mnggunakan Perulangan While</h2>";
 	
-$i = 0;
-while ($i < 5){
- 
-	echo "Hai,Dunia! <br>";
+$i = 1;
+while ($i < 6){
+
+	echo "Hai, ini baris ke - $i <br>";
 	$i++;
 
 }
