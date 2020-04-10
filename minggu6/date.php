e64d596bc32943e799c207ef5a222797fb6e11fb
unknown <anggikhusnurida00@gmail.com>
2020-04-10 22:06:36 +0700

    perintah6

--- minggu6/date.php
+++ minggu6/date.php
@@ -1,7 +1,8 @@
 <?php
-// date untuk menampilkan tanggal dengan format tertentu
 		echo date(
-			"l,d-M-Y",
-			mktime (0, 0, 0, 06, 27, 2000)
+			"l",
+			strtotime (
+				"27 june 2030"
+			) 
 		);
 ?>
