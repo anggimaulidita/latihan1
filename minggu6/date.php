59ab38236b39551b55d4ba1b78904532e8eb90ff
unknown <anggikhusnurida00@gmail.com>
2020-04-10 21:21:09 +0700

    perintah4

--- minggu6/date.php
+++ minggu6/date.php
@@ -2,8 +2,8 @@
 // date untuk menampilkan tanggal dengan format tertentu
 		echo date(
 			"l,d-M-Y",
-			time() +
-				60 * 60 * 60 * 24 * 1000
+			time() -
+				60 * 60 * 60 * 24 * 500
 		);
 // dengan format tersebut kita dapat mengetahui 1000 hari akan datang hari apa 
 ?>
